e71cb64103710990c4d61d138fc2b6269c3ea9be
neillsom <neill.somerville@gmail.com>
2018-08-07 19:25:12 -0700

    formatting

--- src/components/question.js
+++ src/components/question.js
@@ -7,66 +7,67 @@ import "../index.css"
 import "./scripts/question.css";
 
 class Question extends Component {
-	onSubmit = (event) => {
-	event.preventDefault();
-	
-	let correctAnswer = this.correctAnswer;
-	let userAnswer = event.target.userInput.value.toLowerCase();
-	this.props.dispatch(postAnswer({
-		answer: userAnswer}));
-	event.target.userInput.value="";
-	}
-	
-	
-	render(){
-	
-	const feedbackData = (this.props.feedback===undefined || this.props.answered===false ) ? null : (
-	<div className="feedbackboard">
-	<p>{this.props.feedback.feedback}. The answer is: {this.props.feedback.answer}</p>
-	<br />
-	<p>You answered correctly {this.props.feedback.correctTries} out of {this.props.feedback.totalTries} guesses for this card</p>
-	<br />
-	<p>You answered correctly {this.props.correctScore} out of {this.props.totalScore} guesses for this session</p>
-	</div>
-	);
+  onSubmit = (event) => {
+    event.preventDefault();
 
-	return (
-	 
-		<div className="questionboard">
-			<form onSubmit= {event => {this.onSubmit(event), this.props.dispatch(toggleAnswered())}}>
+    let correctAnswer = this.correctAnswer;
+    let userAnswer = event.target.userInput.value.toLowerCase();
+    this.props.dispatch(postAnswer({
+      answer: userAnswer
+    }));
+    event.target.userInput.value = "";
+  }
 
 
-			<div className="question-img-container" >
-				<img key={this.props.id} src={this.props.question} alt="medicinal herbs" />
-			</div>
+  render() {
 
-			{(this.props.answered === true) ? null : 
-			<div className="conditional-input-submit">
-				<input className="userInput"
-					type="text"
-					name="userInput"
-				/>
-				<button className="button-submit">Submit</button>
-			</div>
-			}
-		</form>
-		<div className="feedback">
-			{feedbackData}
-		</div>
-	</div>
-	)
-	}
+    const feedbackData = (this.props.feedback === undefined || this.props.answered === false) ? null : (
+      <div className="feedbackboard">
+        <p>{this.props.feedback.feedback}. The answer is: {this.props.feedback.answer}</p>
+        <br />
+        <p>You answered correctly {this.props.feedback.correctTries} out of {this.props.feedback.totalTries} guesses for this card</p>
+        <br />
+        <p>You answered correctly {this.props.correctScore} out of {this.props.totalScore} guesses for this session</p>
+      </div>
+    );
+
+    return (
+
+      <div className="questionboard">
+        <form onSubmit={event => { this.onSubmit(event), this.props.dispatch(toggleAnswered()) }}>
+
+
+          <div className="question-img-container" >
+            <img key={this.props.id} src={this.props.question} alt="medicinal herbs" />
+          </div>
+
+          {(this.props.answered === true) ? null :
+            <div className="conditional-input-submit">
+              <input className="userInput"
+                type="text"
+                name="userInput"
+              />
+              <button className="button-submit">Submit</button>
+            </div>
+          }
+        </form>
+        <div className="feedback">
+          {feedbackData}
+        </div>
+      </div>
+    )
+  }
 }
 
-const mapStateToProps =state => ({
-	questions: state.questions,
-	correctAnswer: state.correctAnswer,
-	answered: state.protectedData.answered,
-	question: state.protectedData.data.image,
-	id: state.protectedData.id,
-	feedback: state.protectedData.feedback,
-	totalScore: state.protectedData.totalScore,
-	correctScore: state.protectedData.correctScore
+const mapStateToProps = state => ({
+  questions: state.questions,
+  correctAnswer: state.correctAnswer,
+  answered: state.protectedData.answered,
+  question: state.protectedData.data.image,
+  id: state.protectedData.id,
+  feedback: state.protectedData.feedback,
+  totalScore: state.protectedData.totalScore,
+  correctScore: state.protectedData.correctScore
 });
 
 export default requiresLogin()(connect(mapStateToProps)(Question));
